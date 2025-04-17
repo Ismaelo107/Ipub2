30bd8b1d63f4ba321d32cfb9dea3f74f3de9fb48
Ismaelo107 <96046256+Ismaelo107@users.noreply.github.com>
2025-04-17 02:26:16 +0100

    1) He simplificado la ruta de home 2) He creado la migración y el modelo del Stock

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    //
+    protected $table = 'stocks';
+
+    protected $fillable = [
+        //'category_id',
+        'nombre',
+        'unidades',
+        'precio_venta',
+        'precio_compra',
+        'descripcion',
+    ];
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use App\Livewire\ComandaComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::view('/', 'home')->name('home');
 Route::get('/comanda/{mesa}', ComandaComponent::class)->name('comanda');
